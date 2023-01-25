56f23bd53066e2623a5a78836604801f3c514ed5
abcd-arl <lingga.abdulrahman.u@gmail.com>
2023-01-26 00:58:37 +0800

    remove auto_arima import

--- apis/views.py
+++ apis/views.py
@@ -12,7 +12,7 @@ import datetime
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
-from pmdarima.arima import auto_arima
+# from pmdarima.arima import auto_arima
 from sklearn.metrics import mean_absolute_error, mean_squared_error, mean_absolute_percentage_error
 
 from .models import Case
